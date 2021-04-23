f1eb4abc24506c1764091cfcb5f0ae493c57c737
mali-alin <karjavina.alina@yandex.ru>
2021-04-23 14:57:11 +0300

    Feature profile color added, association corrected in user.rb

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,10 @@
 class Question < ApplicationRecord
   belongs_to :user
-  belongs_to :author, class_name: "User"
+  belongs_to :author, class_name: "User", optional: true
 
   validates :text, presence: true
   validates :text, length: {maximum: 255}
+  validates :author_id, null: false
 
   scope :answered, -> { where.not(answer: nil) }
   scope :unanswered, -> { where(answer: nil) }
